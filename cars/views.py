875c9891f1c4a15a21131f53801fd107195fc264
JaysanJaychiev <jaysan.jaychiev.11@gmail.com>
2022-02-04 02:42:33 +0600

    added CarListView

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,13 @@
 import imp
 from django.shortcuts import render
 from rest_framework import generics
-from cars.serializers import CarDetailSerializer
-
+from cars.serializers import CarDetailSerializer, CarListSerializer
+from cars.models import Car
 
 class CarCreateView(generics.CreateAPIView):
     serializer_class = CarDetailSerializer
+
+
+class CarsListView(generics.ListAPIView):
+    serializer_class = CarListSerializer
+    queryset = Car.objects.all()
